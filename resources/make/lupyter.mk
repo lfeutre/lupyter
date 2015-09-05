d11636bdf046580e4536edccc7daad8df7828a93
Duncan McGreggor <oubiwann@gmail.com>
2015-09-05 17:59:40 -0500

    Updated targets for starting jupyter.

--- resources/make/lupyter.mk
+++ resources/make/lupyter.mk
@@ -1,6 +1,6 @@
 IPY_CFG_DIR = ~/.ipython
 IPY_KERN_DIR = $(IPY_CFG_DIR)/kernels
-LUPY_DIR = $(IPY_KERN_DIR)/lupyter
+LUPY_DIR = $(IPY_KERN_DIR)/lfe
 LUPY = ipython-lfe
 LUPY_FULL = $(LUPY_DIR)/$(LUPY)
 LUPY_JSON = $(LUPY_DIR)/kernel.json
@@ -22,11 +22,13 @@ venv:
 
 $(LUPY_VENV): venv
 	virtualenv $(LUPY_VENV)
+	. $(LUPY_VENV)/bin/activate && \
+	pip install jupyter
 
 console:
-	$(LUPY_VENV)/bin/activate && \
-	jupyter-console --kernel=lfe
+	. $(LUPY_VENV)/bin/activate && \
+	ERL_LIBS=$(ERL_LIBS) jupyter-console --kernel=lfe
 
 notebook:
-	$(LUPY_VENV)/bin/activate && \
-	jupyter-notebook --kernel=lfe
+	. $(LUPY_VENV)/bin/activate && \
+	ERL_LIBS=$(ERL_LIBS) jupyter-notebook --kernel=lfe
